7693bda45eb5dc0851584ed83d9e1eb36ac1f976
BardiaMardan <bardiamardan9@gmail.com>
2025-02-24 19:09:32 +0330

    fix: changed a to link

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link'
 
 const NotFound = () => {
   return (
@@ -8,12 +9,12 @@ const NotFound = () => {
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
       <p className="text-xl text-gray-600 mt-4">Oops! The page you are looking for doesn’t exist.</p>
       <p className="text-lg text-gray-500 mt-2">But don't worry, you can always go back to the homepage.</p>
-      <a
+      <Link
         href="/"
         className="mt-6 text-blue-500 underline hover:text-blue-700"
       >
         Go back home
-      </a>
+      </Link>
     </section>
   );
 };
