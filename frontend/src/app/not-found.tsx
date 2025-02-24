737dbf85b53f12aaa1b937ab7baf09b582899bd9
BardiaMardan <bardiamardan9@gmail.com>
2025-02-24 19:11:31 +0330

    , fixed

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center"
     >
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
-      <p className="text-xl text-gray-600 mt-4">Oops! The page you are looking for doesn’t exist.</p>
+      <p className="text-xl text-gray-600 mt-4">Oops! The page you are looking for doesn't exist.</p>
       <p className="text-lg text-gray-500 mt-2">But don't worry, you can always go back to the homepage.</p>
       <Link
         href="/"
