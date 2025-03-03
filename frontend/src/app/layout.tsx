e0a76321a47aec9c31fba9264971bc4c0b11123b
BardiaMardan <bardiamardan9@gmail.com>
2025-03-03 15:14:31 +0330

    removed metadate for bug fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,21 +3,21 @@ import "./globals.css";
 import Layout from "@/components/layout";
 import { getServices } from "@/actions/service.actions";
 
-export async function generateMetadata() {
-  const settings = await getSettings();
+// export async function generateMetadata() {
+//   const settings = await getSettings();
 
-  return {
-    title: `${settings.site_name} | ${settings.site_main_title}`,
-    description: settings.site_description,
-    openGraph: {
-      title: `${settings.site_name} | ${settings.site_main_title}`,
-      description: settings.site_description,
-      url: `${settings.domain}`,
-      images: [{ url: settings.image }],
-      type: 'website',
-    },
-  };
-}
+//   return {
+//     title: `${settings.site_name} | ${settings.site_main_title}`,
+//     description: settings.site_description,
+//     openGraph: {
+//       title: `${settings.site_name} | ${settings.site_main_title}`,
+//       description: settings.site_description,
+//       url: `${settings.domain}`,
+//       images: [{ url: settings.image }],
+//       type: 'website',
+//     },
+//   };
+// }
 
 export default async function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   const services = await getServices()
